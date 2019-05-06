c9301efad1de5bf86d3a1f0ba0959a73ba5d261b
LeiShenVictoria <18811708103@163.com>
2019-05-06 11:47:09 +0800

    Update train.py

--- train.py
+++ train.py
@@ -120,7 +120,7 @@ data_path=args.data_path+args.dataset+'/'
 corpus = getattr(data, args.dataset+'Corpus')(data_path, wordvec_path=args.data_path+'glove.twitter.27B.200d.txt', wordvec_dim=config['emb_size'])
 dials = corpus.get_dialogs()
 metas = corpus.get_metas()
-train_dial, valid_dial, test_dial = dials.get("train"), dials.get("valid"), dials.get("test")
+train_dial, valid_dial, test_dial = dials.get("train"), dials.get("valid"), dials.get("test") # 字典，通过get(key)得到value
 train_meta, valid_meta, test_meta = metas.get("train"), metas.get("valid"), metas.get("test")
 train_loader = getattr(data, args.dataset+'DataLoader')("Train", train_dial, train_meta, config['maxlen'])
 valid_loader = getattr(data, args.dataset+'DataLoader')("Valid", valid_dial, valid_meta, config['maxlen'])
